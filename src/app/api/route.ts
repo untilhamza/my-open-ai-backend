6d4f49cff1fd9809d818dc325bb331796130177c
Hamza <untilhamza@gmail.com>
2023-06-19 18:02:20 +0900

    test without options

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -10,27 +10,9 @@ if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
 }
 
-export async function OPTIONS(request: Request) {
-  const allowedOrigin = request.headers.get("origin");
-  const response = new NextResponse(null, {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": allowedOrigin || "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
-      "Access-Control-Max-Age": "86400",
-    },
-  });
-
-  return response;
-}
-
-// export async function GET(request: Request) {
+// export async function OPTIONS(request: Request) {
 //   const allowedOrigin = request.headers.get("origin");
-//   console.log("GET found origin ", allowedOrigin);
-
-//   const response = new NextResponse("Hello, Next.js!", {
+//   const response = new NextResponse(null, {
 //     status: 200,
 //     headers: {
 //       "Access-Control-Allow-Origin": allowedOrigin || "*",
@@ -40,6 +22,7 @@ export async function OPTIONS(request: Request) {
 //       "Access-Control-Max-Age": "86400",
 //     },
 //   });
+
 //   return response;
 // }
 
