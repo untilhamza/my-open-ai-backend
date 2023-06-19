c5fe4c85ab7410d52f400477aa76786d864c6c71
Hamza <untilhamza@gmail.com>
2023-06-19 18:11:57 +0900

    test without post headers

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -85,13 +85,13 @@ export async function POST(req: Request): Promise<Response> {
   const stream = await OpenAIStream(payload);
   const response = new NextResponse(stream, {
     status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": allowedOrigin || "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
-      "Access-Control-Max-Age": "86400",
-    },
+    // headers: {
+    //   "Access-Control-Allow-Origin": allowedOrigin || "*",
+    //   "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    //   "Access-Control-Allow-Headers":
+    //     "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+    //   "Access-Control-Max-Age": "86400",
+    // },
   });
 
   return response;
