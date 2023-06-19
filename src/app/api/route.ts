480cef551353deb25f3cf297ab57b8737871750a
Hamza <untilhamza@gmail.com>
2023-06-19 11:15:53 +0900

    update options URL headers

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -20,6 +20,8 @@ export async function OPTIONS(request: Request) {
       status: 200,
       headers: {
         "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
       },
     })
   );
