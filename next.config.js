a2f247699c12d974bd9bc62bed63684405be25cc
Hamza <untilhamza@gmail.com>
2023-06-19 11:53:47 +0900

    allow cors in next js config

--- next.config.js
+++ next.config.js
@@ -23,6 +23,30 @@
 //   },
 // };
 
+// module.exports = {
+//   async headers() {
+//     return [
+//       {
+//         // matching all API routes
+//         source: "/api/:path*",
+//         headers: [
+//           // { key: "Access-Control-Allow-Credentials", value: "true" },
+//           { key: "Access-Control-Allow-Origin", value: "*" },
+//           {
+//             key: "Access-Control-Allow-Methods",
+//             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+//           },
+//           {
+//             key: "Access-Control-Allow-Headers",
+//             value: "*",
+//           },
+//         ],
+//       },
+//     ];
+//   },
+// };
+
+// next.config.js
 module.exports = {
   async headers() {
     return [
@@ -30,7 +54,7 @@ module.exports = {
         // matching all API routes
         source: "/api/:path*",
         headers: [
-          // { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
           {
             key: "Access-Control-Allow-Methods",
@@ -38,7 +62,8 @@ module.exports = {
           },
           {
             key: "Access-Control-Allow-Headers",
-            value: "*",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
           },
         ],
       },
