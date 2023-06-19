e84c6b957093e7185222168e5e55ebe08309fe03
Hamza <untilhamza@gmail.com>
2023-06-19 17:50:42 +0900

    remove get request

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -10,44 +10,8 @@ if (!process.env.OPENAI_API_KEY) {
   throw new Error("Missing env var from OpenAI");
 }
 
-const whitelist = ["http://localhost:3000", "https://localhost:3000"];
-
-function setCorsHeaders(response: NextResponse, origin: string | null) {
-  response.headers.set("Access-Control-Allow-Origin", origin || "*");
-  response.headers.set(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, OPTIONS"
-  );
-  response.headers.set(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization"
-  );
-  response.headers.set("Access-Control-Max-Age", "86400");
-  console.log("response in cor func ", response);
-  console.log("origin in cors func", origin);
-  // allow all subdomains of 'slid.cc'
-  // if (origin?.endsWith(".slid.cc")) {
-  //   whitelist.push(origin);
-  // }
-
-  // if (!origin || whitelist.indexOf(origin) !== -1) {
-  //   response.headers.set("Access-Control-Allow-Origin", origin || "*");
-  //   response.headers.set(
-  //     "Access-Control-Allow-Methods",
-  //     "GET, POST, PUT, DELETE, OPTIONS"
-  //   );
-  //   response.headers.set(
-  //     "Access-Control-Allow-Headers",
-  //     "Content-Type, Authorization"
-  //   );
-  //   response.headers.set("Access-Control-Max-Age", "86400");
-  // }
-}
-
 export async function OPTIONS(request: Request) {
   const allowedOrigin = request.headers.get("origin");
-  console.log("OPTIONS found origin ", allowedOrigin);
-
   const response = new NextResponse(null, {
     status: 200,
     headers: {
@@ -62,22 +26,22 @@ export async function OPTIONS(request: Request) {
   return response;
 }
 
-export async function GET(request: Request) {
-  const allowedOrigin = request.headers.get("origin");
-  console.log("GET found origin ", allowedOrigin);
+// export async function GET(request: Request) {
+//   const allowedOrigin = request.headers.get("origin");
+//   console.log("GET found origin ", allowedOrigin);
 
-  const response = new NextResponse("Hello, Next.js!", {
-    status: 200,
-    headers: {
-      "Access-Control-Allow-Origin": allowedOrigin || "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers":
-        "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
-      "Access-Control-Max-Age": "86400",
-    },
-  });
-  return response;
-}
+//   const response = new NextResponse("Hello, Next.js!", {
+//     status: 200,
+//     headers: {
+//       "Access-Control-Allow-Origin": allowedOrigin || "*",
+//       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+//       "Access-Control-Allow-Headers":
+//         "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
+//       "Access-Control-Max-Age": "86400",
+//     },
+//   });
+//   return response;
+// }
 
 export async function POST(req: Request): Promise<Response> {
   const { newTransriptBlock, previousTranscriptBlocks, previousNotes } =
@@ -135,9 +99,6 @@ export async function POST(req: Request): Promise<Response> {
   };
 
   const allowedOrigin = req.headers.get("origin");
-
-  console.log("post found origin ", allowedOrigin);
-
   const stream = await OpenAIStream(payload);
   const response = new NextResponse(stream, {
     status: 200,
